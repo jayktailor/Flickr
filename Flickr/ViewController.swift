a4e234f24521cab0d15cc1cfc617599d1fbded4e
jayktailor <github@jktailor.com>
2017-08-01 13:45:29 +0100

    Initial Commit

---
+++ Flickr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flickr
+//
+//  Created by Jay Tailor on 01/08/2017.
+//  Copyright © 2017 Jay Tailor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
